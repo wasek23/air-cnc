28ef3d0730c66cd2867d29f20a49ad64ecdd4f2e
wasek23 <wasekbellah2311@gmail.com>
2020-06-08 00:57:23 +0600

    search not found

--- src/Components/Sections/SearchResult.js
+++ src/Components/Sections/SearchResult.js
@@ -11,6 +11,11 @@ const SearchResult = () => {
     const filterLocation = getHomesData.filter(item => item.location.toLowerCase().includes(loc));
     const homesData = filterLocation.filter(item => item.guests === guests);
 
+    const searchHandle = () => {
+        document.querySelector('section.mainSection').style.display = 'block';
+        document.querySelector('section.searchResultSection').style.display = 'none';
+    }
+
     return (
         <div>
             <div className="searchResultTop">
@@ -19,7 +24,13 @@ const SearchResult = () => {
             </div>
 
             <div className="searchResultPosts">
-                {homesData.map(home => <HomeResultPost homesData={home} key={home.id}></HomeResultPost>)}
+                {homesData.length !== 0 ? homesData.map(home => <HomeResultPost homesData={home} key={home.id}></HomeResultPost>) :
+                    <>
+                        <h1 style={{ marginBottom: "20px" }}>No Result found <button className="btn btn-radius" onClick={searchHandle}>Search Again</button></h1>
+                        <h2 style={{ marginBottom: "15px" }}>Or, Search by, location, guests respectively Glenelg, 2 | Crafers, 4 | Flagstaff, 4 | Stirling, 7 | Adelaide, 4</h2>
+                        <h3>Because, Our data is poor</h3>
+                    </>
+                }
             </div>
         </div>
 
