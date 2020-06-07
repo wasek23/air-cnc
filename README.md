06cfed5e26b0cca2f5ab053711bbf45af2d3c4a9
wasek23 <wasekbellah2311@gmail.com>
2020-06-07 19:58:53 +0600

    readme.md update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # AIR CNC
+#### Air CNC is a booking website
+
+<br />
+
+### Main Featured:
+* Google Web Fonts Included.
+* Dynamic Header.
+* Search Terms.
+* 2 Step Data Filtration.
+
+### Technology Used:
+* React, JavaScript, Sass, Firebase, Google Analytics.
+
+<br />
+
+### [Preview Project](https://air-cnc.web.app/)
--- src/Components/Posts/HomePostDetail.js
+++ src/Components/Posts/HomePostDetail.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SearchContext } from '../../App';
 
 const HomePostDetail = props => {
+    const [location, setLocation, arrivalDate, setArrivalDate, departureDate, setDepartureDate, adultGuest, setAdultGuest, childGuest, setChildGuest, babyGuest, setBabyGuest] = useContext(SearchContext);
+    const guests = adultGuest + childGuest;
+
     const { thumb, img, title } = props.homeData;
 
     return (
@@ -8,14 +12,12 @@ const HomePostDetail = props => {
             <div className="topImg">
                 <img src={thumb} alt={title} />
                 <img src={img} alt={title} />
-                <h3>Hi</h3>
             </div>
 
             <div className="mainContent grid grid2">
                 <div className="content">
                     <h2>{title}</h2>
                 </div>
-
             </div>
         </article>
     );
